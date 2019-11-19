544a299e3cdf28d70a149896f6312021ab69f730
DniGaytan <danielgaytan@MacBook-Pro-de-Daniel.local>
2019-11-19 09:37:32 -0600

    added functions to stores.js

--- Routes/stores.js
+++ Routes/stores.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const store = require('../schemas/store-schema');
+let uuid = require('uuid');
+let parser = require('body-parser');
+let jsonP = parser.json();
 
 //Registers New Store
 router.post('/register-store',function(req,res,next){
@@ -39,8 +42,20 @@ router.post('/register-store',function(req,res,next){
     });
 });
 
+//get all the stores
+router.post('/get', (req, res) => {
+    store.find().then((stores) => {
+        return res.status(200).json(stores);
+    }).catch( (e) => {
+        res.statusMessage = "uups, db cannot be reached";
+                res.status(500).json({
+                    message : res.statusMessage,
+                });
+    });
+});
+
 //get the required store by store id
-router.post('/get/:id', (req, res) => {
+router.post('/get/:id', jsonP, (req, res) => {
     if(req.params.id != req.body.storeId){
         res.statusMessage = "ids does not match";
         return res.status(406).json({
@@ -48,6 +63,24 @@ router.post('/get/:id', (req, res) => {
         });
     }
     store.find({storeId: req.params.storeId}).then((stores) => {
+        return res.status(200).json(stores);
+    }).catch( (e) => {
+        res.statusMessage = "uups, db cannot be reached";
+                res.status(500).json({
+                    message : res.statusMessage,
+                });
+    });
+});
+
+//get the stores by manager id
+router.post('/get-by-manager/:id', jsonP, (req, res) => {
+    if(req.params.id != req.body.managerId){
+        res.statusMessage = "ids does not match";
+        return res.status(406).json({
+            message : res.statusMessage,
+        });
+    }
+    store.find({storeManager: req.params.managerId}).then((stores) => {
         return res.status(200).json(stores);
     }).catch( (e) => {
         res.statusMessage = "uups, db cannot be reached";

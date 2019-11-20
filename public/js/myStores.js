87bfd802c00acf45d20386903d37784bcd786980
JulioRubio <julioarg97@gmail.com>
2019-11-19 21:50:37 -0600

    FindStores and MyStores Navigation

---
+++ public/js/myStores.js
@@ -0,0 +1,97 @@
+function init(){
+    if(document.cookie != ""){
+      //gets only the value of the cookie, without the name
+      var re = new RegExp(name + "=([^;]+)");
+      var value = re.exec(document.cookie);
+      //ajax call to validate if user is already logged in
+      $.ajax({
+        url: '/validate',
+        method:'POST',
+        contentType:'application/json',
+        data:JSON.stringify({
+          Email:value[1]
+        }),
+        error : (response) => { 
+          window.location.replace("../login.html");
+        }
+      });
+    }
+};
+
+init();
+
+$('#logout').on('click', (event) => {
+    event.preventDefault();
+    var re = new RegExp(name + "=([^;]+)");
+    var value = re.exec(document.cookie);
+    $.ajax({
+        url: '/logout',
+        method:'POST',
+        contentType:'application/json',
+        data:JSON.stringify({
+          Email:value[1]
+        }),
+        success:(response) =>{
+            window.location.replace("../login.html");
+        }
+      });
+});
+
+$('#findStores').on('click', (event) => {
+    event.preventDefault();
+    var re = new RegExp(name + "=([^;]+)");
+    var value = re.exec(document.cookie);
+    $.ajax({
+        url: '/validate',
+        method:'POST',
+        contentType:'application/json',
+        data:JSON.stringify({
+          Email:value[1]
+        }),
+        success:(response) =>{
+          window.location.href = "../findstores.html";
+        }
+      });
+});
+
+$('#ordersDashboard').on('click', (event) => {
+    event.preventDefault();
+    var re = new RegExp(name + "=([^;]+)");
+    var value = re.exec(document.cookie);
+    $.ajax({
+        url: '/validate',
+        method:'POST',
+        contentType:'application/json',
+        data:JSON.stringify({
+          Email:value[1]
+        }),
+        success:(response) =>{
+            window.location.href = "../dashboard.html";
+        },
+        error:(response)=>{
+            window.location.replace("../login.html");
+        }
+      });
+});
+
+
+$('#home').on('click', (event) => {
+    event.preventDefault();
+    var re = new RegExp(name + "=([^;]+)");
+    var value = re.exec(document.cookie);
+    $.ajax({
+        url: '/validate',
+        method:'POST',
+        contentType:'application/json',
+        data:JSON.stringify({
+          Email:value[1]
+        }),
+        success:(response) =>{
+            window.location.href = "../home.html";
+        },
+        error:(response)=>{
+            window.location.replace("../login.html");
+        }
+      });
+});
+

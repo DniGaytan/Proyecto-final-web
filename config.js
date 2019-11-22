1b2ea72044ae0959102a74d7167ef0b58be45556
Daniel Gaytan <dnigaytan07@gmail.com>
2019-11-22 02:23:55 -0600

    online config

---
+++ config.js
@@ -0,0 +1,2 @@
+exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost/store-garden";
+exports.PORT = process.env.PORT || 8080;
--- server.js
+++ server.js
@@ -42,7 +42,48 @@ app.use(carts);
 app.use(cookieParser());
 
 
+let server;
 
-app.listen(8080,function(){
-    console.log('Server running on port ' + 8080);
-});
+function runServer(port, databaseUrl){
+	return new Promise( (resolve, reject ) => {
+		mongoose.connect(databaseUrl, response => {
+			if ( response ){
+				return reject(response);
+			}
+			else{
+				server = app.listen(port, () => {
+					console.log( "App is running on port " + port );
+					resolve();
+				})
+				.on( 'error', err => {
+					mongoose.disconnect();
+					return reject(err);
+				})
+			}
+		});
+	});
+}
+
+function closeServer(){
+	return mongoose.disconnect()
+		.then(() => {
+			return new Promise((resolve, reject) => {
+				console.log('Closing the server');
+				server.close( err => {
+					if (err){
+						return reject(err);
+					}
+					else{
+						resolve();
+					}
+				});
+			});
+		});
+}
+
+runServer( PORT, DATABASE_URL )
+	.catch( err => {
+		console.log( err );
+	});
+
+module.exports = { app, runServer, closeServer };
